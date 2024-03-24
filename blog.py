a47af50048480d04b3100b39ce36166931c4fe63
Amal-Dahshan <amaldahshan11710@gmail.com>
2024-03-24 20:32:25 +0200

    new

--- blog.py
+++ blog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 posts = [
@@ -12,7 +12,19 @@ posts = [
         'author': 'Jane Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'date_posted': 'April 21, 2018'
+        'date_posted': 'April 21, 2019'
+    },
+    {
+        'author': 'Blake Schafer',
+        'title': 'Blog Post 3',
+        'content': 'First post content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted': 'April 20, 2020'
+    },
+    {
+        'author': 'Jake Doe',
+        'title': 'Blog Post 4',
+        'content': 'Second post content. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'date_posted': 'April 21, 2022'
     }
 ]
 
@@ -22,10 +34,14 @@ def home():
     return render_template("home.html", posts=posts)
 
 
-@app.route("/about")
-def about():
-    return "<h1>About Page</h1>"
+@app.route("/movies")
+def movies():
+    return render_template("movies.html")
 
+
+@app.route("/appliances")
+def appliances():
+    return render_template("appliances.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
